44e63fd7908010d98fd448226e539672b7bd23ee
CheezItMan <chris@adadevelopersacademy.org>
2018-08-30 23:34:03 -0700

    add argumenterror for illegal rental length

--- lib/rental.rb
+++ lib/rental.rb
@@ -1,12 +1,14 @@
 module GreenBox
   class Rental
     attr_reader :movie, :date_range, :customer
+    DAILY_COST = 3.0
 
     def initialize(movie, date_range, customer)
+      raise ArgumentError, "Illegal Date Range" if date_range.nights < 1
       @movie = movie
       @date_range = date_range
       @customer = customer
-      @cost = 3
+      @cost = DAILY_COST
     end
 
     def cost
