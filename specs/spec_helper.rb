25074a74bf7746b287a1319deaef56f91b66a5f2
CheezItMan <chris@adadevelopersacademy.org>
2018-08-30 20:16:10 -0700

    initial commit

---
+++ specs/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'time'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+require 'awesome_print'
+# Add simplecov
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# Require_relative your lib files here!
