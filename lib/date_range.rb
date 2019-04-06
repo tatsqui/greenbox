9832defbc2cec1a01eb5b65e0f883307ad8e4681
Tatiana Quintana <tatsqui@gmail.com>
2019-04-05 20:04:15 -0700

    wrote date_range class methods, all provided tests green

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -2,6 +2,28 @@ require 'time'
 
 module GreenBox
   class DateRange
+    class DateRangeError < StandardError; end
+    attr_reader :start_date, :end_date, :nights
+
+    def initialize(start_date, end_date)
+      raise ArgumentError, "Given start: #{start_date}, end: #{end_date} " if end_date < start_date 
+
+      @start_date = start_date
+      @end_date = end_date
+    end
+
+    def contains(date)
+      return (start_date...end_date).include?(date) #not inclusive of last day
+    end
+
+    def overlaps(other)
+      return start_date < other.end_date && end_date >= other.start_date #made this not inclusive of last day
+    end
+
+    def nights
+      return ((@end_date - start_date) / 86400).to_i
+    end
 
   end
 end
+
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -2,7 +2,7 @@ require 'time'
 require_relative 'spec_helper'
 
 
-xdescribe 'GreenBox::DateRange' do
+describe 'GreenBox::DateRange' do
 
   describe 'initialization' do
     it 'can be created' do
