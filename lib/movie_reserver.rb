91b944191a807ac2b19181f09a6ee0f830df4947
Tatiana Quintana <tatsqui@gmail.com>
2019-04-05 23:08:01 -0700

    corrected #available_movies and #rent_movie

--- lib/movie_reserver.rb
+++ lib/movie_reserver.rb
@@ -12,12 +12,25 @@ module GreenBox
     end
 
     def available_movies(date_range)
-      # movies_available = @rentals.select { |rental| rental.date_range.overlaps(date_range) }
-      # return @movies - movies_available
+      available_movies = @movies.map { |movie| movie.title }
+
+      currently_rented = @rentals.select { |rental| rental.overlaps(date_range) }
+      unavailable_movies = currently_rented.map { |rental| rental.movie }
+
+      available_movies -= unavailable_movies
+      return available_movies
     end
 
     def rent_movie(movie_title, date_range, customer_name)
+      unless @movies.include?(movie_title)
+        raise StandardError, "Movie title: #{movie_title} is currently unavailable at this GreenBox"
+      end
 
+      unless available_movies(date_range).include?(movie_title)
+        raise StandardError, "Your requested movie is not reservable for dates: #{date_range}"
+      end
+        
+      @rentals << Rental.new(movie_title, date_range, customer_name)
     end
 
     private
--- lib/rental.rb
+++ lib/rental.rb
@@ -5,9 +5,9 @@ module GreenBox
 
     def initialize movie, date_range, customer_name
       if date_range.start_date >= date_range.end_date
-        raise ArgumentError, "Rental period must be atleast 24 hours, given: #{date_range.start_date}, #{date_range.end_date}"
+        raise ArgumentError, "Rental period must be at least 24 hours, given: #{date_range.start_date}, #{date_range.end_date}"
       end
-
+      
       @movie = movie
       @date_range = date_range
       @customer_name = customer_name
