538479249e734023d61bf6423b984e9fa62b8f78
Tatiana Quintana <tatsqui@gmail.com>
2019-04-05 20:11:31 -0700

    added some clarifying comments for instance methods in date_range

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -11,14 +11,17 @@ module GreenBox
       @end_date = end_date
     end
 
+    #returns true if a date is within another date range
     def contains(date)
-      return (start_date...end_date).include?(date) #not inclusive of last day
+      return (start_date...end_date).include?(date) # not inclusive of last day
     end
 
+    # returns true if a date overlaps
     def overlaps(other)
-      return start_date < other.end_date && end_date >= other.start_date #made this not inclusive of last day
+      return start_date < other.end_date && end_date >= other.start_date # overlap not inclusive of last day
     end
 
+    # accurate count of rental nights
     def nights
       return ((end_date - start_date) / 86400).to_i
     end
