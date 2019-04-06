3d97ac4718b1ca0313f812e67aa0d7fb6bc18d6d
Tatiana Quintana <tatsqui@gmail.com>
2019-04-05 21:06:08 -0700

    added movie reserver class and .load_movies

--- lib/movie_reserver.rb
+++ lib/movie_reserver.rb
@@ -1,7 +1,24 @@
 require 'csv'
+require_relative "movie"
+require_relative "rental"
 
 module GreenBox
   class MovieReserver
+    attr_reader
 
+    def initialize movies, rentals
+      @movies = movies.load_movies
+      @rentals = []
+    end
+
+
+
+    private
+
+    def self.load_movies
+        return CSV.read("data/movies.csv", headers: false).map do |line|
+          self.new(line[0].to_i, line[1], line[2], line[3].split(":")
+        end
+    end
   end
 end
